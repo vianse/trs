e060201c737b70a77ebf23ca08129597365f4978
Juan Carlos Santiago <krlos013@gmail.com>
2016-04-07 14:47:37 -0500

    Inicio 2

--- db/schema.rb
+++ db/schema.rb
@@ -47,12 +47,12 @@ ActiveRecord::Schema.define(version: 20150324234448) do
   create_table "consultoras", force: true do |t|
     t.string   "email",              default: "", null: false
     t.string   "encrypted_password", default: "", null: false
-    t.integer  "sign_in_count",      default: 0
+    t.string  "sign_in_count",      default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",    default: 0
+    t.string  "failed_attempts",    default: 0
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.datetime "created_at"
@@ -142,7 +142,7 @@ ActiveRecord::Schema.define(version: 20150324234448) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.string  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
